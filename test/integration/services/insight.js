d699fea93b78b7dead7afcf0d1f61489e57f119e
Charles Hill <chill@degreesofzero.com>
2018-04-18 14:15:34 +0200

    Check tx data from insight API

--- test/integration/services/insight.js
+++ test/integration/services/insight.js
@@ -49,6 +49,20 @@ describe('services.insight', function() {
 					}
 
 					try {
+						expect(receivedData).to.be.an('object');
+						expect(receivedData.txid).to.be.a('string');
+						expect(receivedData.vout).to.be.an('array');
+						_.each(receivedData.vout, function(vout) {
+							expect(vout).to.be.an('object');
+							var addresses = _.keys(vout);
+							_.each(addresses, function(address) {
+								expect(address).to.be.a('string');
+							});
+							var amounts = _.values(vout);
+							_.each(amounts, function(amount) {
+								expect(amount).to.be.a('number');
+							});
+						});
 					} catch (error) {
 						return done(error);
 					}
