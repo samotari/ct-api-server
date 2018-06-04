3385f5b837b2ee06bb8169332f6e1c504fd108e2
Charles Hill <chill@degreesofzero.com>
2018-06-04 16:13:48 +0200

    More unit tests for Insight

--- test/integration/sockets/unsubscribe.js
+++ test/integration/sockets/unsubscribe.js
@@ -33,5 +33,31 @@ describe('socket', function() {
 		it('can unsubscribe from a channel', function(done) {
 			client.unsubscribe(channel, done);
 		});
+
+		describe('with other client(s) subscribed', function() {
+
+			var anotherClient;
+			beforeEach(function(done) {
+				anotherClient = manager.socketClient();
+				anotherClient.socket.once('open', function() {
+					done();
+				});
+			});
+
+			beforeEach(function(done) {
+				anotherClient.subscribe(channel, done);
+			});
+
+			it('only unsubscribes the client that requested to be unsubscribed', function(done) {
+				client.unsubscribe(channel, function(error) {
+					if (error) return done(error);
+					expect(!_.isEmpty(app.sockets.subscriptions[channel])).to.equal(true);
+					anotherClient.socket.id(function(id) {
+						expect(app.sockets.subscriptions[channel][id]).to.not.be.undefined;
+						done();
+					});
+				});
+			});
+		});
 	});
 });
