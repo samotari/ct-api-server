17ef194f8913cc962a7b122e6b09ff0087201bc1
Charles Hill <chill@degreesofzero.com>
2018-08-07 14:11:53 +0200

    Removed block-cypher, block-io, and insight services

--- services/index.js
+++ services/index.js
@@ -4,10 +4,7 @@ module.exports = function(app) {
 
 	return {
 		bitcoindZeroMQ: require('./bitcoind-zeromq')(app),
-		blockCypher: require('./block-cypher')(app),
-		blockIO: require('./block-io')(app),
 		coinbase: require('./coinbase')(app),
-		insight: require('./insight')(app),
 		poloniex: require('./poloniex')(app),
 		onionMoneroBlockchainExplorer: require('./onion-monero-blockchain-explorer')(app),
 	};
